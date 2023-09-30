ef7ebc51c5c118871cf322ddb936c56b3a5a6c56
CalvinRA <10659444@my.uvu.edu>
2023-09-30 13:37:53 -0600

    Began work on GameAction and GameActionHandeler

--- DAGV 2023 Fall Semester/Assets/Scripts/AnimStatesBehaviour.cs
+++ DAGV 2023 Fall Semester/Assets/Scripts/AnimStatesBehaviour.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -6,7 +8,7 @@ public class AnimStatesBehaviour : StateMachineBehaviour
     public UnityEvent onEnterEvent;
     public UnityEvent onUpdateEvent;
     public UnityEvent onExitEvent;
-
+    
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         // Custom logic here
