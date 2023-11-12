3372f03a560378fee7610d7c0ca9a7ec7e58396a
CalvinRA <10659444@my.uvu.edu>
2023-11-11 20:43:53 -0700

    Assignment 7 continue

---
+++ Maya Python Scripts/DAGV 2440 Assignment 7.py
@@ -0,0 +1,23 @@
+import maya.cmds as cmds
+
+def change_color(color):
+    selection = cmds.ls(selection=True)
+
+    if not selection:
+        cmds.error("Select object before running ChangeColor().")
+
+    if color < 0 or color > 31:
+        color = 0
+        cmds.warning("Color argument must be within the range of 0 to 31. Value defaulted to 0")
+
+    for obj in selection:
+        shapes = cmds.listRelatives(obj, shapes=True) or []
+
+        for shape in shapes:
+            # Enable the override for color
+            cmds.setAttr(shape + ".overrideEnabled", 1)
+            # Set the override color
+            cmds.setAttr(shape + ".overrideColor", color)
+
+# Call the function with a color value (you can change it)
+change_color(1)
